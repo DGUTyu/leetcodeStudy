aa95fa511b9b0c0d84d0c213a62917711dcb093a
yu <2912435404@qq.com>
2021-11-09 11:50:42 +0800

    init project

---
+++ src/com/wxdgut/chap1/HelloLeetCode.java
@@ -0,0 +1,13 @@
+package com.wxdgut.chap1;
+
+/**
+ * @author Administrator
+ * @program: leetcodeStudy
+ * @date 2021-11-09 11:16:47
+ */
+
+public class HelloLeetCode {
+    public static void main(String[] args) {
+        System.out.println("hello,LeetCode!");
+    }
+}
